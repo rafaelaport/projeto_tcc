184782069ca9224728967ceab094b4c2fede7c42
WELLINGTON FIDELIS DA SILVEIRA <wellingtonf20@gmail.com>
2021-09-10 19:19:08 -0300

    Development helpers file

--- web/js/pages/cadastrar.js
+++ web/js/pages/cadastrar.js
@@ -1,4 +1,8 @@
-var cpf = "99999999999";
+/**
+ * By Wellington Fidelis
+ * at 08/2021
+ * 
+ */
 /* Verifica, ao carregar a página, se o campo de cpf/cnpj está preenchido */
 $(document).ready(function () {
   if ($("#inputCpfCnpj").val() === "") {
@@ -31,26 +35,6 @@ $("#buttonAddAnotherDevice").on("click", function (event) {
   handleAddAnotherDevice();
 });
 
-
-function formatCpfCnpj(text) {
-  const badchars = /[^\d]/g
-  const maskCpf = /(\d{3})(\d{3})(\d{3})(\d{2})/
-  const maskCnpj = /(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/
-  const value = new String(text).replace(badchars, "");
-  if (value.length === 11) {
-    return value.replace(maskCpf, "$1.$2.$3-$4");
-  } else if (value.length === 14) {
-    return value.replace(maskCnpj, "$1.$2.$3/$4-$5");
-  } else {
-    return value;
-  }
-}
-
-function removeSpecialCharacters(fieldValue) {
-  let value = new String(fieldValue).replace(/(\-|\/|\.)/g, '');
-  return value;
-}
-
 function handleFormShow() {
   let cpfCnpjUserForm = removeSpecialCharacters($("#inputCpfCnpj").val());
   let idUser = $.ajax({
@@ -73,41 +57,6 @@ function handleFormShow() {
         return response.message;
       }
     });
-}
-
-function validateCpfCnpj(idField) {
-  if (idField.val().length === 11 || idField.val().length === 14 || idField.val().length ===
-    18) {
-    $(idField).val(formatCpfCnpj(idField.val()));
-    $(idField).css("border-color", "#1ab394");
-    return true;
-  } else {
-    $(idField).val(formatCpfCnpj(idField.val()));
-    $(idField).css("border-color", "#ED5565");
-    return false;
-  }
-}
-
-function validateInputsForm(dataAttribute) {
-  let inputValues = $("[" + dataAttribute + "]").get();
-  let result = true;
-  inputValues.forEach((input, index) => {
-    if ($(input).val().length === 0) {
-      $(input).css("border-color", "#ED5565");
-      result = false;
-    } else {
-      $(input).css("border-color", "#1ab394");
-    }
-  });
-  return result;
-}
-
-function clearFields(dataAttribute) {
-  let inputValues = $("[" + dataAttribute + "]").get();
-  inputValues.forEach((input, index) => {
-    $(input).val("");
-    $(input).css("border-color", "#e5e6e7")
-  });
 }
 
 function handleAddDevice() {
