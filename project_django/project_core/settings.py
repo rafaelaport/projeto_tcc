7cfe8b67bbd5ee67fd6b7151d434526bfdf3ae51
WellingtonFidelis <wellingtonf20@gmail.com>
2023-03-05 14:27:27 -0300

    feat(frontend): Home page, Login page e Navbar reorganized

--- project_django/project_core/settings.py
+++ project_django/project_core/settings.py
@@ -129,6 +129,8 @@ MEDIA_URL = 'media/'
 STATIC_ROOT = BASE_DIR / 'staticfiels'
 MEDIA_ROOT = BASE_DIR / 'media '
 
+STATICFILES_DIRS = [BASE_DIR / 'static',]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- project_django/project_core/urls.py
+++ project_django/project_core/urls.py
@@ -21,7 +21,7 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("contas/", include('django.contrib.auth.urls')),
-    path("", TemplateView.as_view(template_name="index.html"), name="index"),
+    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
 ]
 
 
